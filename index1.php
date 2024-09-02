431fa0d79935280ca797e83bf9b2812ce51cb73f
jorgemc94 <jmaciasc1994@gmail.com>
2024-09-02 19:01:40 +0200

    add index1

---
+++ index1.php
@@ -0,0 +1,11 @@
+<?php
+    $rooms = [
+        ['id' => '0','name' => 'Individual Room', 'number' => '101', 'price' => '80', 'discount' => '0'],
+        ['id' => '1','name' => 'Double Room', 'number' => '200', 'price' => '140', 'discount' => '0'],
+        ['id' => '2','name' => 'Premium Room', 'number' => '305', 'price' => '300', 'discount' => '0'],
+    ];
+?>
+
+<pre>
+    <?php print_r($rooms) ?>
+</pre>
