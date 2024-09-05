c0f6c8b57eaed8a11596e96684a7f1d9cb7fbb8d
jorgemc94 <jmaciasc1994@gmail.com>
2024-09-05 09:22:43 +0200

    add index4

---
+++ index4.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $json = file_get_contents('./rooms.json');
+    $rooms = json_decode($json, true);
+    $foundRoom = null;
+    foreach ($rooms as $room) {
+        if ($room['id'] == $id) {
+            $foundRoom = $room;
+            break;
+        }
+    }
+}
+?>
+
+<?php if (isset($foundRoom) && $foundRoom): ?>
+    <h1>Room: <?= htmlspecialchars($foundRoom['roomType']) ?></h1>
+    <ul>
+        <li>Name: <?= htmlspecialchars($foundRoom['roomType']) ?></li>
+        <li>Number: <?= htmlspecialchars($foundRoom['roomNumber']) ?></li>
+        <li>Price: $<?= htmlspecialchars($foundRoom['price']) ?></li>
+        <li>Discount: <?= htmlspecialchars($foundRoom['discount']) ?>%</li>
+    </ul>
+<?php elseif (isset($id)): ?>
+    <p>id not found</p>
+<?php else: ?>
+    <p>No room id provided.</p>
+<?php endif; ?>
+
